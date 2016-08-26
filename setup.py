da86828c5e55c39f6f6bce70de51c94d683c4277
Julien Cigar <julien@perdition.city>
2016-08-26 10:35:20 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'SQLAlchemy<1.1',
+]
+
+setup(name='sautils',
+      version='0.1',
+      description='sautils',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Topic :: Database",
+        "License :: OSI Approved :: BSD License",
+        ],
+      author='Julien Cigar',
+      author_email='julien@perdition.city',
+      url='https://github.com/silenius/sautils',
+      keywords='sqlalchemy serialize',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      )
